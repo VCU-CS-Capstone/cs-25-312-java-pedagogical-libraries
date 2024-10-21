ca9b36130b9b4fc03b52679ca7565850af92b78e
Derek Chiou <chioudj@vcu.edu>
2024-10-20 22:42:50 -0400

    Setup IntelliJ and test package import

---
+++ src/chioudj/DerekSaysHello.java
@@ -0,0 +1,7 @@
+package chioudj;
+
+public class DerekSaysHello {
+    public static void main(String[] args) {
+        System.out.println("Hello from Derek!");
+    }
+}
---
+++ src/edu/vcu/jpedal/ImportTest.java
@@ -0,0 +1,16 @@
+package edu.vcu.jpedal;
+
+/* Uncomment your import and main call when you add your package! */
+import chioudj.DerekSaysHello;
+// import douttl.LucaSaysHello;
+// import gqasimi.QasimiSaysHello;
+// import westrykj.KennedySaysHello;
+
+public class ImportTest {
+    public static void main(String[] args) {
+        DerekSaysHello.main(new String[]{});
+        // LucaSaysHello.main(new String[]{});
+        // QasimiSaysHello.main(new String[]{});
+        // KennedySaysHello.main(new String[]{});
+    }
+}
