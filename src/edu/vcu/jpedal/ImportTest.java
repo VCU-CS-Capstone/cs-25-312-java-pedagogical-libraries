9fe422f7852d397b41253a602ec44c1b7da0b184
Luca Doutt <luca.doutt03@gmail.com>
2024-11-07 18:50:50 -0500

    Add eclipse stuff to gitignore, Luca says hi

--- src/edu/vcu/jpedal/ImportTest.java
+++ src/edu/vcu/jpedal/ImportTest.java
@@ -3,13 +3,13 @@ package edu.vcu.jpedal;
 /* Uncomment your import and main call when you add your package! */
 import chioudj.DerekSaysHello;
 import westrykj.KennedySaysHello;
-// import douttl.LucaSaysHello;
+import douttl.LucaSaysHello;
 // import gqasimi.QasimiSaysHello;
 
 public class ImportTest {
     public static void main(String[] args) {
         DerekSaysHello.main(new String[]{});
-        // LucaSaysHello.main(new String[]{});
+        LucaSaysHello.main(new String[]{});
         // QasimiSaysHello.main(new String[]{});
         KennedySaysHello.main(new String[]{});
     }
