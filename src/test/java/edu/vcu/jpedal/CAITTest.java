12d18f5e00a45f467f40298a911a0a7b9dd7cce7
Derek Chiou <chioudj@vcu.edu>
2025-02-24 20:15:56 -0500

    Restructure project to JUnit standard

---
+++ src/test/java/edu/vcu/jpedal/CAITTest.java
@@ -0,0 +1,11 @@
+package edu.vcu.jpedal;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CAITTest {
+
+    @Test
+    void nodesAreEqual() {
+    }
+}
