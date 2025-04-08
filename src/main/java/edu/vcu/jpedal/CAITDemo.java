48aaac56dc842853540aee08788774440d9d96cc
Derek Chiou <chioudj@vcu.edu>
2025-04-08 02:37:37 -0400

    Add prompting to demo

--- src/main/java/edu/vcu/jpedal/CAITDemo.java
+++ src/main/java/edu/vcu/jpedal/CAITDemo.java
@@ -1,42 +1,60 @@
 package edu.vcu.jpedal;
 
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 public class CAITDemo {
+    String intendedOutput = """
+    Welcome to the CAIT demo!
+    Available files for testing:
+    [1] CorrectMath.java
+    [2] RenamedCorrectMath.java
+    etc.
+    Enter a number [1-2]:
+    > 2
+    The file is as follows:
+    (code)
+    Choose from predetermined patterns or enter a custom one?
+    [1] Predetermined
+    [2] Custom
+    Enter a number [1-2]:
+    > 2
+    Available patterns:
+    [1] for-each loop
+    [2] recursive function
+    Which would you like to match? Enter a number [1-2]:
+    > 1
+    Searching...
+    No matches found | Matches: (list them)
+    """;
     public static void main(String[] args) {
-        /*
-         * Welcome to the CAIT demo!
-         * Available files for testing:
-         * [1] CorrectMath.java
-         * [2] RenamedCorrectMath.java
-         * etc.
-         * Which would you like to search? Enter a number [1-2]:
-         * > 2
-         * The file is as follows:
-         * (code)
-         * Enter a pattern [1] or choose a predetermined pattern [2]?
-         * > 2
-         * Available patterns:
-         * [1] for-each loop
-         * [2] recursive function
-         * Which would you like to match? Enter a number [1-2]:
-         * > 1
-         * Searching...
-         * No matches found | Matches: (list them)
-         */
         Scanner userIn = new Scanner(System.in);
-        while(true) {
-            List<String> tempList = new ArrayList<>();
-            tempList.add("yo");
-            tempList.add("hey");
-            int a = promptList(userIn, tempList, "What is it?");
-            System.out.println(a*2);
+        System.out.println("Welcome to the CAIT demo!");
+
+        List<String> filesAvailable = getSourceSamples();
+        if(filesAvailable.isEmpty()) {
+            System.out.println("No sample code files found. Exiting...");
+            return;
         }
+        int selection = promptList(userIn, filesAvailable, "Sample code files available:");
+        System.out.printf("TODO: selection %s%n", selection);
+        // TODO: get corresponding file and parse
+        selection = promptList(userIn, List.of("Predetermined", "Custom"), "Choose from predetermined patterns or enter a custom one?");
+        System.out.printf("TODO: selection %s%n", selection);
+        // TODO: pattern based on selection
+        // TODO: print matches
+        userIn.close();
     }
-    
+
+    private static List<String> getSourceSamples() {
+        List<String> out = new ArrayList<String>();
+        out.add("haha.java");
+        return out;
+    }
+
     private static int promptList(Scanner scanner, List<String> options, String prompt) {
         System.out.println(prompt);
         for (int i = 0; i < options.size(); i++) {
