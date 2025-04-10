1e44fc8452a70b6acfcdde5ae0fb02af69f44435
Derek Chiou <chioudj@vcu.edu>
2025-04-10 03:33:37 -0400

    Remove redundant tests (moved to JUnit)

--- src/main/java/edu/vcu/jpedal/SampleScript.java
+++ src/main/java/edu/vcu/jpedal/SampleScript.java
@@ -11,20 +11,5 @@ public class SampleScript {
                 "public class SuperAwesome { public static void main(String[] args) { int x = 3; } }");
         assert parsed != null;
         System.out.println(parsed);
-
-        final String PATH_TO_SAMPLES = "src/test/resources/";
-
-        Node correctNode = CAIT.parseFile(PATH_TO_SAMPLES + "CorrectMath.java");
-        assert correctNode != null;
-        Node renamedCorrectNode = CAIT.parseFile(PATH_TO_SAMPLES + "RenamedCorrectMath.java");
-        assert renamedCorrectNode != null;
-        Node wackyNode = CAIT.parseFile(PATH_TO_SAMPLES + "WackyMath.java");
-        assert wackyNode != null;
-        // This should report EQUAL once nodesAreEqual is complete -dc
-        System.out.println("correct and renamedCorrect are: " + (CAIT.nodesAreEqual(correctNode, renamedCorrectNode) ? "EQUAL" : "NOT EQUAL"));
-        // This should report NOT EQUAL once nodesAreEqual is complete -dc
-        System.out.println("correct and wacky are: " + (CAIT.nodesAreEqual(correctNode, wackyNode) ? "EQUAL" : "NOT EQUAL"));
-        // This should report EQUAL once nodesAreEqual is complete - ld
-        System.out.println("correct and correct are: " + (CAIT.nodesAreEqual(correctNode, correctNode) ? "EQUAL" : "NOT EQUAL"));
     }
 }
